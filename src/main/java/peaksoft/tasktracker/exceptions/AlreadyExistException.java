6dca7bade21b576a506438bbafb3e507fbdc3776
Matmusa ab <abduvohobuulumatmusa@gmail.com>
2023-07-12 21:32:32 +0600

    add exception classes

---
+++ src/main/java/peaksoft/tasktracker/exceptions/AlreadyExistException.java
@@ -0,0 +1,10 @@
+package peaksoft.tasktracker.exceptions;
+
+public class AlreadyExistException extends RuntimeException{
+    public AlreadyExistException() {
+    }
+
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+}
