22273a094e4896df1cb17ce77dccd81e2d821101
Matmusa ab <abduvohobuulumatmusa@gmail.com>
2023-07-19 14:19:53 +0600

    add email sender

--- src/main/java/peaksoft/tasktracker/config/SecurityConfig.java
+++ src/main/java/peaksoft/tasktracker/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,22 +23,24 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
         @Bean
         SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http
-                    .csrf()
-                    .disable()
-                    .authorizeHttpRequests()
-                    .requestMatchers("/api/auth/**", "/swagger-ui/**","/v3/api-docs/**")
-                    .permitAll()
-                    .anyRequest()
-                    .permitAll()
-                    .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests((authorizeHttpRequests) ->
+                            authorizeHttpRequests
+                                    .requestMatchers(
+                                            "/api/auth/**",
+                                            "/swagger-ui/**",
+                                            "v3/api-docs/**",
+                                            "/auth/**"
+                                    )
+                                    .permitAll()
+                                    .anyRequest()
+                                    .authenticated())
+                    .sessionManagement((sessionManagement) ->
+                            sessionManagement
+                                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authenticationProvider(authenticationProvider)
                     .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
             return http.build();
-
-
         }
 
     }
