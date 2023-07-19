963ca633892ebf4db25e8f996470d568ae46a58d
Malik <asanbekovmalik2@gmail.com>
2023-07-19 13:20:03 +0600

    Merge pull request #6 from matmusa/feature/add_dto_classes
    create dto classes

--- src/main/java/peaksoft/tasktracker/repository/UserRepository.java
+++ src/main/java/peaksoft/tasktracker/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package peaksoft.tasktracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import peaksoft.tasktracker.dto.response.UserResponse;
+
+
 import peaksoft.tasktracker.entity.User;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
